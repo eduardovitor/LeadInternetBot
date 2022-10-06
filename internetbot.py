d903ea7eb1351887f2885f2f8d36f306403239c9
Eduardo Vítor <64266915+eduardovitor@users.noreply.github.com>
2022-10-06 10:01:13 -0300

    Atualizando nome do bot

--- internetbot.py
+++ internetbot.py
@@ -9,11 +9,11 @@ try:
     st = speedtest.Speedtest()
     down_speed = st.download()
     down_speed_megabits= str(round(down_speed/1000000, 2))
-    msg="Olá eu sou o NetBot \n" + "A velocidade atual da internet do Lead é de {} Mbps".format(down_speed_megabits)
+    msg="Olá eu sou o InternetBot \n" + "A velocidade atual da internet do Lead é de {} Mbps".format(down_speed_megabits)
     client = slack.WebClient(token=SLACK_TOKEN)
     client.chat_postMessage(channel=CHANNEL,text=msg)
 except:
-    msg="Olá eu sou o NetBot \n" + "Não consegui calcular a velocidade da internet devido a uma falha no meu servidor"
+    msg="Olá eu sou o InternetBot \n" + "Não consegui calcular a velocidade da internet devido a uma falha no meu servidor"
     client = slack.WebClient(token=SLACK_TOKEN)
     client.chat_postMessage(channel=CHANNEL,text=msg)
 
